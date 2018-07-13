2d9536534ad17025a24e1bb6c5707ba1650fbea2
Kevin <kevin@rocher-dev.com>
2018-07-13 15:54:40 +0200

    v 0.13.2
    * Add more languages to config.rb

--- example-config.rb
+++ example-config.rb
@@ -1,7 +1,7 @@
 # Theme config
 integento_theme_id = "Mytheme"
 integento_theme_lang = "fr_FR"
-integento_theme_lang2 = "en_US"
+integento_theme_lang_supp = ["en_EN"]
 
 # Magento 2 config
 integento_css_dir = "pub/static/frontend/" + integento_theme_id + "/default/"
@@ -9,7 +9,6 @@ integento_css_dir = "pub/static/frontend/" + integento_theme_id + "/default/"
 # Compass config
 http_path = "/"
 css_dir = integento_css_dir + integento_theme_lang + "/css"
-css2_dir = integento_css_dir + integento_theme_lang2 + "/css"
 sass_dir = "app/design/frontend/" + integento_theme_id + "/default/styles"
 images_dir = "assets/images"
 javascripts_dir = "assets/js"
@@ -44,8 +43,10 @@ end
 # Copy to a secondary dir
 require 'fileutils'
   on_stylesheet_saved do |file|
-      if File.exists?(file)
-      new_file = css2_dir + "/" + File.basename(file, File.extname(file)) + File.extname(file);
+  if File.exists?(file)
+    integento_theme_lang_supp.each do |i|
+      new_file = integento_css_dir + i + "/css/" + File.basename(file, File.extname(file)) + File.extname(file);
       FileUtils.cp(file, new_file);
+    end
   end
 end
